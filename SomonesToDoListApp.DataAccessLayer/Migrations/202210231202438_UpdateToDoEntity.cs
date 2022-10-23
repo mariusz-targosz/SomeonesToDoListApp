8871bece55e611e77a8197c82cd9792851f8c99b
Mariusz Targosz <mata@softwaremind.com>
2022-10-23 17:41:55 +0200

    Add migration to meet updated ToDo entity

---
+++ SomonesToDoListApp.DataAccessLayer/Migrations/202210231202438_UpdateToDoEntity.cs
@@ -0,0 +1,33 @@
+﻿namespace SomeonesToDoListApp.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateToDoEntity : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.ToDo");
+            AddColumn("dbo.ToDo", "Title", c => c.String());
+            AddColumn("dbo.ToDo", "Description", c => c.String());
+            AddColumn("dbo.ToDo", "CreatedAt", c => c.DateTime(nullable: false));
+            AddColumn("dbo.ToDo", "CreatedBy", c => c.Guid(nullable: false));
+            DropColumn("dbo.ToDo", "Id");
+            AddColumn("dbo.ToDo", "Id", c => c.Guid(nullable: false));
+            AddPrimaryKey("dbo.ToDo", "Id");
+            DropColumn("dbo.ToDo", "ToDoItem");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.ToDo", "ToDoItem", c => c.String(nullable: false));
+            DropPrimaryKey("dbo.ToDo");
+            AlterColumn("dbo.ToDo", "Id", c => c.Int(nullable: false, identity: true));
+            DropColumn("dbo.ToDo", "CreatedBy");
+            DropColumn("dbo.ToDo", "CreatedAt");
+            DropColumn("dbo.ToDo", "Description");
+            DropColumn("dbo.ToDo", "Title");
+            AddPrimaryKey("dbo.ToDo", "Id");
+        }
+    }
+}
