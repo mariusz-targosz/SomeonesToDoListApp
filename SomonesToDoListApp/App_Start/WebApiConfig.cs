f39fb3bab9bd386d15b302d973b0d3a6bb54d044
Mariusz Targosz <mata@softwaremind.com>
2022-10-23 12:44:29 +0200

    Add camel case property names contract resolver for json formatter

--- SomonesToDoListApp/App_Start/WebApiConfig.cs
+++ SomonesToDoListApp/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace SomeonesToDoListApp
 {
@@ -14,11 +15,21 @@ namespace SomeonesToDoListApp
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            SetupJsonFormatter(config);
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        private static void SetupJsonFormatter(HttpConfiguration config)
+        {
+            var formatters = config.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
