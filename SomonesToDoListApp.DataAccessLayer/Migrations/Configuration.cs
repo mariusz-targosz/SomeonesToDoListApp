81cbb23a1c86257ba36ed506aabcd3e8a8c43373
Mariusz Targosz <mata@softwaremind.com>
2022-10-23 17:52:02 +0200

    Code clean-up

--- SomonesToDoListApp.DataAccessLayer/Migrations/Configuration.cs
+++ SomonesToDoListApp.DataAccessLayer/Migrations/Configuration.cs
@@ -1,33 +1,22 @@
 ﻿using SomeonesToDoListApp.DataAccessLayer.Context;
 using SomeonesToDoListApp.DataAccessLayer.Entities;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SomeonesToDoListApp.DataAccessLayer.Migrations
 {
     internal sealed class Configuration : DbMigrationsConfiguration<SomeonesToDoListContext>
     {
-        /// <inheritdoc />
-        /// <summary>
-        /// Configuring the migrations behaviour in Entity Framework
-        /// </summary>
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        /// <inheritdoc />
-        /// <summary>
-        /// A seed method for a layer of base data for the to do list
-        /// </summary>
-        /// <param name="someonesToDoListContext"></param>
         protected override void Seed(SomeonesToDoListContext someonesToDoListContext)
         {
-            if (someonesToDoListContext.ToDos.Any()) return;
+            if (someonesToDoListContext.ToDos.Any())
+                return;
 
             var toDo = new ToDo(Guid.NewGuid(), "Feed my dog", string.Empty, DateTime.UtcNow, Guid.NewGuid());
             someonesToDoListContext.ToDos.AddOrUpdate(toDo);
