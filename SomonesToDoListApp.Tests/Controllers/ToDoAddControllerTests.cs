cf78b04e19c7e4a425c93c76d5f4103b9bb6d595
Mariusz Targosz <mata@softwaremind.com>
2022-10-24 23:19:31 +0200

    Add tests for Add and Delete controller

---
+++ SomonesToDoListApp.Tests/Controllers/ToDoAddControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using AutoMapper;
+using NSubstitute;
+using Shouldly;
+using SomeonesToDoListApp.Controllers;
+using SomeonesToDoListApp.DataAccessLayer.Entities;
+using SomeonesToDoListApp.DataAccessLayer.Repositories;
+using SomeonesToDoListApp.DataAccessLayer.ValueObjects;
+using SomeonesToDoListApp.Requests;
+using SomeonesToDoListApp.Services.Services;
+using Xunit;
+
+namespace SomeonesToDoListApp.Tests.Controllers
+{
+    public class ToDoAddControllerTests
+    {
+        private readonly ToDo _toDo;
+        private readonly ToDoAddRequest _toDoAddRequest;
+
+        private readonly IToDoRepository _toDoRepository;
+
+        private readonly ToDoController _sut;
+
+        public ToDoAddControllerTests()
+        {
+            _toDo = new ToDo(Guid.NewGuid(), new ToDoTitle("Buy milk"), "Remember to buy it twice", DateTime.UtcNow, Guid.NewGuid());
+            _toDoAddRequest = new ToDoAddRequest(_toDo.Title.ToString(), _toDo.Description);
+            var toDoResponse = new ToDoResponse(_toDo.Id, _toDo.Title.ToString(), _toDo.Description);
+
+            var toDoFactory = Substitute.For<IToDoFactory>();
+            toDoFactory.Create(_toDoAddRequest.Title, _toDoAddRequest.Description, Arg.Any<Guid>())
+                .Returns(_toDo);
+
+            _toDoRepository = Substitute.For<IToDoRepository>();
+
+            var mapper = Substitute.For<IMapper>();
+            mapper.Map<ToDoResponse>(_toDo)
+                .Returns(toDoResponse);
+
+            _sut = new ToDoController(toDoFactory, _toDoRepository, mapper);
+        }
+
+        [Fact]
+        public async Task AddAsync_WhenCalled_ReturnsToDo()
+        {
+            // Act
+            var result = await _sut.AddAsync(_toDoAddRequest, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<CreatedNegotiatedContentResult<ToDoResponse>>();
+
+            var response = result as CreatedNegotiatedContentResult<ToDoResponse>;
+            response.ShouldNotBeNull();
+
+            var toDoResponse = response.Content;
+            toDoResponse.ShouldNotBeNull();
+            toDoResponse.Id.ShouldNotBe(Guid.Empty);
+            toDoResponse.Title.ShouldBe(_toDo.Title.ToString());
+            toDoResponse.Description.ShouldBe(_toDo.Description);
+        }
+
+        [Fact]
+        public async Task AddAsync_WhenCalled_CallsAddAsync()
+        {
+            // Act
+            var _ = await _sut.AddAsync(_toDoAddRequest, CancellationToken.None);
+
+            // Assert
+            await _toDoRepository.Received()
+                .AddAsync(_toDo, Arg.Any<CancellationToken>());
+
+            await _toDoRepository.ReceivedWithAnyArgs()
+                .AddAsync(default, default);
+        }
+
+        [Fact]
+        public async Task AddAsync_WhenCalled_SetsLocationHeader()
+        {
+            // Act
+            var result = await _sut.AddAsync(_toDoAddRequest, CancellationToken.None);
+
+            // Assert
+            var response = result as CreatedNegotiatedContentResult<ToDoResponse>;
+            response.ShouldNotBeNull();
+
+            var toDoResponse = response.Content;
+            response.Location.ToString().ShouldBe($"api/todos/{toDoResponse.Id}");
+        }
+    }
+}
---
+++ SomonesToDoListApp.Tests/Controllers/ToDoDeleteControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using NSubstitute;
+using SomeonesToDoListApp.Controllers;
+using SomeonesToDoListApp.DataAccessLayer.Entities;
+using SomeonesToDoListApp.DataAccessLayer.Repositories;
+using SomeonesToDoListApp.DataAccessLayer.ValueObjects;
+using SomeonesToDoListApp.Services.Services;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web.Http.Results;
+using Shouldly;
+using Xunit;
+
+namespace SomeonesToDoListApp.Tests.Controllers
+{
+    public class ToDoDeleteControllerTests
+    {
+        private readonly ToDo _toDo;
+
+        private readonly IToDoRepository _toDoRepository;
+
+        private readonly ToDoController _sut;
+
+        public ToDoDeleteControllerTests()
+        {
+            _toDo = new ToDo(Guid.NewGuid(), new ToDoTitle("Buy milk"), "Remember to buy it twice", DateTime.UtcNow, Guid.NewGuid());
+
+            _toDoRepository = Substitute.For<IToDoRepository>();
+            var toDoFactory = Substitute.For<IToDoFactory>();
+            var mapper = Substitute.For<IMapper>();
+
+            _sut = new ToDoController(toDoFactory, _toDoRepository, mapper);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ToDoExists_ReturnsNoContent()
+        {
+            // Arrange
+            _toDoRepository.GetByIdAsync(_toDo.Id, CancellationToken.None)
+                .Returns(_toDo);
+
+            // Act
+            var result = await _sut.DeleteAsync(_toDo.Id, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<StatusCodeResult>();
+
+            var statusCodeResult = result as StatusCodeResult;
+            statusCodeResult.ShouldNotBeNull();
+            statusCodeResult.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ToDoDoesNotExist_ReturnsNotFound()
+        {
+            // Act
+            var result = await _sut.DeleteAsync(_toDo.Id, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<NotFoundResult>();
+        }
+    }
+}
