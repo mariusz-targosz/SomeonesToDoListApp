90d3fe915b5ada236b054e521dfac38a4af01728
Mariusz Targosz <mata@softwaremind.com>
2022-10-24 22:47:17 +0200

    Extend ToDoTitle with ToString method

--- SomonesToDoListApp.DataAccessLayer/ValueObjects/ToDoTitle.cs
+++ SomonesToDoListApp.DataAccessLayer/ValueObjects/ToDoTitle.cs
@@ -19,6 +19,8 @@ namespace SomeonesToDoListApp.DataAccessLayer.ValueObjects
             Value = value;
         }
 
+        public override string ToString() => Value;
+
         protected override IEnumerable<object> GetEqualityComponents()
         {
             yield return Value;
